5538f2317006bfb481569d1d9f849c6b90b92b4b
Abdul Kemal <abdulkemalnw@gmail.com>
2020-08-13 10:40:14 +0700

    feat: add blueprint of repository interface

--- app/Contracts/Repositories/User/AddressRepository.php
+++ app/Contracts/Repositories/User/AddressRepository.php
@@ -4,4 +4,7 @@ namespace App\Contracts\Repositories\User;
 
 interface AddressRepository
 {
+    public function create(array $data);
+
+    public function updateById(array $data, $id);
 }
--- app/Contracts/Repositories/User/UserRepository.php
+++ app/Contracts/Repositories/User/UserRepository.php
@@ -4,4 +4,9 @@ namespace App\Contracts\Repositories\User;
 
 interface UserRepository
 {
+    public function paginate($perPage = 10, $columns = ['*'], $pageName = 'page', $page = null);
+
+    public function create(array $data);
+
+    public function updateById(array $data, $id);
 }
